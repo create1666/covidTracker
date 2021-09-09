19be519f5b8ae045345649be0f72d3fa80382d71
create1666 <create1666@gmail.com>
2021-09-09 19:43:33 +0100

    fetch

--- src/App.js
+++ src/App.js
@@ -5,20 +5,58 @@ import { useEffect, useState } from "react";
 function App() {
   const [countries, setCountries] = useState([]);
 
+  // useEffect(() => {
+  //   const getCountriesData = async () => {
+  //     await fetch("https://disease.sh/v3/covid-19/countries", {
+  //       mode: "no-cors",
+  //       method: "get",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     })
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         const countries = data.map((country) => {
+  //           return {
+  //             name: country.country,
+  //             value: country.countryInfo.iso3,
+  //           };
+  //         });
+  //         setCountries(countries);
+  //         console.log(countries);
+  //       })
+  //       .catch();
+  //   };
+  //   getCountriesData();
+  // }, []);
+
   useEffect(() => {
     const getCountriesData = async () => {
-      await fetch("https://disease.sh/v3/covid-19/countries")
-        .then((response) => response.json())
-        .then((data) => {
-          const countries = data.map((country) => {
-            return {
-              name: country.country,
-              value: country.countryInfo.iso3,
-            };
-          });
-          setCountries(countries);
-          console.log(countries);
+      try {
+        const response = await fetch(
+          "https://disease.sh/v3/covid-19/countries",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        let countries = await response.json();
+
+        countries = countries.map((country) => {
+          return {
+            name: country.country,
+            value: country.countryInfo.iso3,
+          };
         });
+
+        setCountries(countries);
+        console.log(countries);
+      } catch (e) {
+        console.log(e);
+      }
     };
     getCountriesData();
   }, []);
