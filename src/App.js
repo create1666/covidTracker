1bd881d934da8dddc70e838dd4809639f90d3c73
create1666 <create1666@gmail.com>
2021-09-10 23:40:05 +0100

    fixed the drop down state

--- src/App.js
+++ src/App.js
@@ -4,68 +4,78 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [countries, setCountries] = useState([]);
-
-  // useEffect(() => {
-  //   const getCountriesData = async () => {
-  //     await fetch("https://disease.sh/v3/covid-19/countries", {
-  //       mode: "no-cors",
-  //       method: "get",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //     })
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         const countries = data.map((country) => {
-  //           return {
-  //             name: country.country,
-  //             value: country.countryInfo.iso3,
-  //           };
-  //         });
-  //         setCountries(countries);
-  //         console.log(countries);
-  //       })
-  //       .catch();
-  //   };
-  //   getCountriesData();
-  // }, []);
-
+  const [selectedCountry, setSelectedCountry] = useState("worldwide");
   useEffect(() => {
-    const getCountriesData = async () => {
-      try {
-        const response = await fetch(
-          "https://disease.sh/v3/covid-19/countries",
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-
-        let countries = await response.json();
-
-        countries = countries.map((country) => {
-          return {
-            name: country.country,
-            value: country.countryInfo.iso3,
-          };
-        });
-
-        setCountries(countries);
-        console.log(countries);
-      } catch (e) {
-        console.log(e);
-      }
+    const getCountriesData = () => {
+      fetch("https://disease.sh/v3/covid-19/countries", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => {
+            return {
+              name: country.country,
+              value: country.countryInfo.iso3,
+            };
+          });
+          setCountries(countries);
+          console.log(countries);
+        })
+        .catch();
     };
     getCountriesData();
   }, []);
+
+  const onhandleChange = (e) => {
+    const countryCode = e.target.value;
+    console.log("i love u:", countryCode);
+    setSelectedCountry(countryCode);
+  };
+
+  // useEffect(() => {
+  //   const getCountriesData = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         "https://disease.sh/v3/covid-19/countries",
+  //         {
+  //           method: "GET",
+  //           headers: {
+  //             "Content-Type": "application/json",
+  //           },
+  //         }
+  //       );
+
+  //       let countries = await response.json();
+
+  //       countries = countries.map((country) => {
+  //         return {
+  //           name: country.country,
+  //           value: country.countryInfo.iso3,
+  //         };
+  //       });
+
+  //       setCountries(countries);
+  //       console.log(countries);
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //   };
+  //   getCountriesData();
+  // }, []);
   return (
     <div className="app">
       <div className="app_header">
         <h1>COVID-19 TRACKER</h1>
         <FormControl>
-          <Select variant="outlined" value="">
+          <Select
+            onChange={onhandleChange}
+            variant="outlined"
+            value={selectedCountry}
+          >
+            <MenuItem value="worldwide">worldwide</MenuItem>
+
             {countries.map((country, index) => (
               <MenuItem key={index} value={country.value}>
                 {country.name}
