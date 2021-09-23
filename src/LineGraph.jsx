574eeffc4b58f42493ad9f074e24cd296ba6922e
create1666 <create1666@gmail.com>
2021-09-24 00:00:29 +0100

    chart integration

--- src/LineGraph.jsx
+++ src/LineGraph.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./LineGraph.css";
+import { Line } from "react-chartjs-2";
+
 const LineGraph = () => {
-  return <div className="chart"></div>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    try {
+      const getChartData = async () => {
+        let response = await fetch(
+          "https://astro-cors-server.herokuapp.com/fetch/https://disease.sh/v3/covid-19/historical/all?lastdays=120"
+        );
+        let responses = await response.json().then((response) => {
+          console.log(response);
+        });
+        setData(responses);
+      };
+      getChartData();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  return (
+    <div className="chart">
+      <Line data={data} />
+    </div>
+  );
 };
 
 export default LineGraph;
