a87e2c33ba2fb3fa9057e6aa14c3e28b778a352a
create1666 <create1666@gmail.com>
2021-09-24 20:50:55 +0100

    worked on the chart logic

--- src/LineGraph.jsx
+++ src/LineGraph.jsx
@@ -3,26 +3,45 @@ import "./LineGraph.css";
 import { Line } from "react-chartjs-2";
 
 const LineGraph = () => {
+  const chartDataTransformed = (data) => {
+    const newChartData = [];
+    let lastDatapoint;
+    for (let date in data.cases) {
+      if (lastDatapoint) {
+        let newDataPoint = {
+          x: date,
+          y: data["cases"][date] - lastDatapoint,
+        };
+        newChartData.push(newDataPoint);
+      }
+      lastDatapoint = data["cases"][date];
+    }
+    return newChartData;
+  };
+
   const [data, setData] = useState([]);
   useEffect(() => {
     try {
       const getChartData = async () => {
-        let response = await fetch(
+        await fetch(
           "https://astro-cors-server.herokuapp.com/fetch/https://disease.sh/v3/covid-19/historical/all?lastdays=120"
-        );
-        let responses = await response.json().then((response) => {
-          console.log(response);
-        });
-        setData(responses);
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            let newChartData = chartDataTransformed(data);
+            setData(newChartData);
+          });
       };
       getChartData();
     } catch (error) {
       console.log(error);
     }
   }, []);
+
   return (
     <div className="chart">
-      <Line data={data} />
+      <Line data={{ data }} />
     </div>
   );
 };
